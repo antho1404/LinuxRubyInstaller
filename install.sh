5cd84a36c2390015cc62c8f6e87b5c317387f943
antho1404 <anthony.estebe@gmail.com>
2012-10-21 01:21:50 +0200

    prepare multiple environment selection

--- install.sh
+++ install.sh
@@ -1,18 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 VERSION="1.9.3"
+ENVIRONMENT="rvm"
 DATABASES=[]
 DIR=`dirname $0`
 
 usage() {
-  echo usage: $0 options
   echo ""
-  echo This script install ruby using rvm.
+  echo "usage: $0 options"
   echo ""
-  echo options:
-  echo -h  Help
-  echo -v  Specify the version of ruby you want \($VERSION by default\)
-  echo -d  Databases you want to install \(empty by default\) \[postgresql,mysql,mongodb,sqlite3\]
+  echo "This script install ruby on Debian based distributions."
+  echo ""
+  echo "options:"
+  echo "  -h  Help"
+  # echo "  -e  Specify the environment for ruby ($ENVIRONMENT by default) [rvm,rbenv]"
+  echo "  -v  Specify the version of ruby you want ($VERSION by default)"
+  echo "  -d  Databases you want to install (empty by default) [postgresql,mysql,mongodb,sqlite3]"
 }
 
 install_dependencies() {
@@ -33,13 +36,38 @@ install_rvm() {
   source ~/.bash_profile
 }
 
-install_ruby() {
-  # install ruby 
-  rvm install $VERSION
+install_rbenv() {
+  git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
 
-  # use the installed version as default
-  rvm use $VERSION --default
+  mkdir -p ~/.rbenv/plugins
+  git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins
   
+  echo '' >> ~/.bashrc
+  echo '### Load rbenv environment' >> ~/.bashrc
+  echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+  echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+  exec $SHELL
+}
+
+install_ruby() {
+  if [ $ENVIRONMENT = "rvm" ]
+  then
+    # install ruby 
+    rvm install $VERSION
+
+    # use the installed version as default
+    rvm use $VERSION --default
+  fi
+
+  if [ $ENVIRONMENT = "rbenv" ]
+  then
+    rbenv install $VERSION
+    rbenv rehash
+    rbenv global $VERSION
+  fi
+}
+
+install_bundler() {
   # install soft to checkout and install all the gems
   gem install bundler
 }
@@ -51,12 +79,19 @@ install_databases() {
   done
 }
 
-while getopts "hv:d:" OPTION
+
+
+
+while getopts "he:v:d:" OPTION
 do
   case $OPTION in
     h)
       usage
       exit 1
+      ;;
+    e)
+      # uncomment to test using rbenv
+      #ENVIRONMENT=$OPTARG
       ;;
     v)
       VERSION=$OPTARG
@@ -77,10 +112,26 @@ then
   exit 1
 fi
 
-install_dependencies
-install_rvm
-install_ruby
-install_bundler
-install_databases
+echo "You will install ruby $VERSION using $ENVIRONMENT and $DATABASES adaptaters"
+echo "Are you agree ? (Y/n)"
+read answer
 
-echo 'YOUPIIII TOP COOOL'
+if [ $answer = "Y" ]
+then
+  install_dependencies
+  if [ $ENVIRONMENT = "rvm" ]
+  then
+    install_rvm
+  fi
+  if [ $ENVIRONMENT = "rbenv" ]
+  then
+    install_rbenv
+  fi
+  install_ruby
+  install_bundler
+  install_databases
+
+  echo 'YOUPIIII TOP COOOL'
+else
+  usage
+fi
